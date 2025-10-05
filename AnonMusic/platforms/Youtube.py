c41660d13267121ba71a89e6871a61d3341a6263
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-10-05 15:15:18 +0530

    Update Youtube.py

--- AnonMusic/platforms/Youtube.py
+++ AnonMusic/platforms/Youtube.py
@@ -17,9 +17,6 @@ import logging
 import aiohttp
 import config
 from config.config import API_URL, VIDEO_API_URL, API_KEY
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots5b2d9a')
 
 def cookie_txt_file():
     cookie_dir = f"{os.getcwd()}/cookies"
